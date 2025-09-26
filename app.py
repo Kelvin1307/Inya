a5ce52a59b7d728534f9d16bed92f0ff76c11b6d
Timothy  Kelvin <kingkelvin1307@gmail.com>
2025-09-26 17:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # Register all Blueprints
 app.register_blueprint(sentiment_bp, url_prefix='/v2/senti')
 app.register_blueprint(policy_bp, url_prefix='/v1/policy')
+app.register_blueprint(msg_bp, url_prefix='/v3/sms')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
