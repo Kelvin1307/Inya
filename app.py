835d76b4a26fcf96348b889f2fef0b61019b7d3e
Timothy  Kelvin <kingkelvin1307@gmail.com>
2025-09-26 14:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from sentiment import sentiment_bp
-from msg import msg_bp
 from policy_parser import policy_bp
 
 app = Flask(__name__)
 
 # Register all Blueprints
 app.register_blueprint(sentiment_bp)
-app.register_blueprint(msg_bp)
-app.register_blueprint(policy_bp)
+app.register_blueprint(policy_bp, url_prefix='/v1/policy')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
