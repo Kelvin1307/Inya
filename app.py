3625dc57103dfcc6c4b23f7833d5de339c0465bd
Timothy  Kelvin <kingkelvin1307@gmail.com>
2025-09-27 19:49:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from sentiment import sentiment_bp
-from msg import msg_bp
 from policy_parser import policy_bp
-from payments import payments_bp
+import os
 
 app = Flask(__name__)
-
-# Register blueprints with prefixes
-app.register_blueprint(sentiment_bp, url_prefix="/v1/sentiment")
-app.register_blueprint(msg_bp, url_prefix="/v1/msg")
-app.register_blueprint(policy_bp, url_prefix="/v1/policy")
-app.register_blueprint(payments_bp, url_prefix="/v1/payments")
+app.register_blueprint(sentiment_bp, url_prefix='/v1/nlp')
+app.register_blueprint(policy_bp, url_prefix='/v1/policy')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
